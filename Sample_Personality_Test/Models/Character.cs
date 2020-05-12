99f5fca916624b7a570771e7e8f4fe3a1a8a78a4
rsanders001 <rsanders2k@gmail.com>
2020-05-12 17:55:27 -0500

    Homework 13 Commit

---
+++ Sample_Personality_Test/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace MarvelDemo.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconPath { get; set; }
+        public int SeriesId { get; set; }
+        public Image Thumbnail { get; set; }
+    }
+}
---
+++ Sample_Personality_Test/Services/MarvelDataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MarvelDemo.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MarvelDemo.Services
+{
+    public class MarvelDataService : IMarvelDataService
+    {
+        // TODO: Add your Marvel Developer Account keys
+        const string _API_PRIVATE_KEY = "0463bfd6e5f6dcedc66c649ef6ace7e7945f9940";
+        const string _API_PUBLIC_KEY = "3c44404df57490c24193af5c63450ae1";
+
+        readonly IHashService _hashService;
+
+        public MarvelDataService(IHashService hashService)
+        {
+            _hashService = hashService;
+        }
+
+        public async Task<IEnumerable<Comic>> GetComicsBySeries(int seriesId, string orderBy = null)
+        {
+            var ts = Guid.NewGuid().ToString();
+            var hash = _hashService.CreateMd5Hash(ts + _API_PRIVATE_KEY + _API_PUBLIC_KEY);
+
+            if (string.IsNullOrWhiteSpace(orderBy))
+                orderBy = "issueNumber";
+
+            var url =
+                $@"http://gateway.marvel.com/v1/public/series/{seriesId}/comics?orderBy={orderBy}&apikey={_API_PUBLIC_KEY}&hash={hash}&ts={ts}";
+            
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+            
+            var responseObject = JObject.Parse(response);
+
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<IEnumerable<Comic>>(responseObject["data"]["results"].ToString()));
+        }
+    }
+}
